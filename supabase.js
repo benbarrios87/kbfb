ff75bf5bb172d30760c24409ed842895af09dd15
benbarrios87 <benbarrios87@gmail.com>
2026-06-15 14:21:12 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,4 @@
+const SUPABASE_URL = "https://otsaqlidvnlwsdvfhcqq.supabase.co";
+const SUPABASE_KEY = "sb_publishable_GnxwJ05PVpzN-MBGTBpI0Q_3HsQU0nb";
 
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
