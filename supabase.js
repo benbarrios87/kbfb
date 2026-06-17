75f5631e52b8bbf7c86404473cdfd0fce8abc4f7
benbarrios87 <benbarrios87@gmail.com>
2026-06-17 11:54:25 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,3 +2,16 @@ const SUPABASE_URL = "https://otsaqlidvnlwsdvfhcqq.supabase.co";
 const SUPABASE_KEY = "sb_publishable_GnxwJ05PVpzN-MBGTBpI0Q_3HsQU0nb";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+async function testSupabase() {
+  const { data, error } = await supabaseClient
+    .from("kbfb_events")
+    .select("*")
+    .limit(5);
+
+  console.log("SUPABASE TEST");
+  console.log(data);
+  console.log(error);
+}
+
+testSupabase();
